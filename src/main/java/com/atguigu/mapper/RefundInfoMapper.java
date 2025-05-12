29ac8d87186cd8c850f828c669294432d8f0a526
hehonghui <3045603277@qq.com>
2025-05-12 10:39:35 +0800

    项目初始化2

---
+++ src/main/java/com/atguigu/mapper/RefundInfoMapper.java
@@ -0,0 +1,9 @@
+package com.atguigu.mapper;
+
+import com.atguigu.payment.entity.RefundInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface RefundInfoMapper extends BaseMapper<RefundInfo> {
+
+
+}
